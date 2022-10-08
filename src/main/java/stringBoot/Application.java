0c7a7ae73c956e938515e246d8c2c0f75a027a67
EnemyButFriend <113972013+EnemyButFriend@users.noreply.github.com>
2022-10-08 15:04:26 +0300

    String Boot

---
+++ src/main/java/stringBoot/Application.java
@@ -0,0 +1,16 @@
+package stringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+        application.run(args);
+    }
+}
